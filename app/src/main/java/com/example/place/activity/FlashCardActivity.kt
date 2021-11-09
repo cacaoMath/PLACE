cd87b3de314a11c050ae475e5627b8d9be359f12
cacaoMath <mahiro.1192@gmail.com>
2021-11-09 15:07:03 +0900

    bugfix : フラッシュカードで音声再生時に自動で次の問題へ行くように対応

--- app/src/main/java/com/example/place/activity/FlashCardActivity.kt
+++ app/src/main/java/com/example/place/activity/FlashCardActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
@@ -64,6 +65,21 @@ class FlashCardActivity : AppCompatActivity(), CardStackListener, TextToSpeech.O
         setContentView(flashCardBinding.root)
         quizSet = Quiz().GetQuizSet(questionNum, getInstance().quizPattern)
         tts = TextToSpeech(this,this)
+
+        tts.setOnUtteranceProgressListener(object: UtteranceProgressListener() {
+            override fun onStart(utteranceId: String?) {
+            }
+
+            override fun onDone(utteranceId: String?) {
+                if(utteranceId.equals("jp")){
+                    cardStackView.swipe()
+                }
+            }
+
+            override fun onError(utteranceId: String?) {
+            }
+
+        })
 
 
         cardStackView = flashCardBinding.cardStackView
@@ -144,9 +160,9 @@ class FlashCardActivity : AppCompatActivity(), CardStackListener, TextToSpeech.O
 
         if(isVoiceMode){
             tts.language = Locale.ENGLISH
-            tts.speak(quizSet[position][1], TextToSpeech.QUEUE_ADD, null, utteranceId)
+            tts.speak(quizSet[position][1], TextToSpeech.QUEUE_ADD, null, "en")
             tts.language = Locale.JAPANESE
-            tts.speak(quizSet[position][6], TextToSpeech.QUEUE_ADD, null, utteranceId)
+            tts.speak(quizSet[position][6], TextToSpeech.QUEUE_ADD, null, "jp")
         }
 
     }
@@ -188,7 +204,8 @@ class FlashCardActivity : AppCompatActivity(), CardStackListener, TextToSpeech.O
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
             if ((tts.isLanguageAvailable(Locale.ENGLISH) >= TextToSpeech.LANG_AVAILABLE)
-                && (tts.isLanguageAvailable(Locale.JAPANESE) >= TextToSpeech.LANG_AVAILABLE)) {
+                && (tts.isLanguageAvailable(Locale.JAPANESE) >= TextToSpeech.LANG_AVAILABLE)
+                && isVoiceMode) {
                 Log.i(TAG, "言語の設定するのに成功しました．")
                 window.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
                 cardStackView.adapter = MyAdapter(quizSet)
